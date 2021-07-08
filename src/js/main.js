bef267344199593f4e717beabc426c10e139874c
Ekaterina <abashkina.k@gmail.com>
2021-07-08 17:57:44 +0300

    fix:change modile version

--- src/js/main.js
+++ src/js/main.js
@@ -3,11 +3,11 @@ class TabList {
   constructor(buttonsContainer, tabs, tabsBtn) {
     this.buttonsContainer = buttonsContainer;
     this.tabs = tabs;
-    
-    
+
+
     this.buttonsContainer.addEventListener('click', event => {
       const index = event.target.closest(`.${tabsBtn}`).dataset.value;
-      document.querySelectorAll(`.${tabsBtn}`).forEach( (btn) => {
+      document.querySelectorAll(`.${tabsBtn}`).forEach((btn) => {
         btn.classList.remove('active')
       })
       event.target.closest(`.${tabsBtn}`).classList.add('active')
@@ -15,21 +15,41 @@ class TabList {
     });
   }
 
-  
+
   openTab(index) {
     this.tabs.querySelector('.active').classList.remove('active');
     this.tabs.querySelector(`.tab--${index}`).classList.add('active');
   }
 }
 
-document.addEventListener('DOMContentLoaded', ()=>{
-  const buttonsContainer = document.querySelector('.tabs__nav');
-  const tabs             = document.querySelector('.tabs');
-  const buttonsContainer2 = document.querySelector('.tab2__btns');
-  const tabs2             = document.querySelector('.tabs2');
+document.addEventListener('DOMContentLoaded', () => {
+  const buttonsContainer = document.querySelector('.tabs__btns');
+  const tabsServices = document.querySelector('.tabs');
+  const tabList = new TabList(buttonsContainer, tabsServices, 'tab__btn');
 
-  
-  const tabList = new TabList(buttonsContainer, tabs,'tabs__btn');
-  const tabList2 = new TabList(buttonsContainer2, tabs2,'tab2__btn');
+  const buttonsContainerProjects = document.querySelector('.tabs__nav');
+  const tabsProjects = document.querySelector('.section-projects__tabs');
+  const tabListProjects = new TabList(buttonsContainerProjects, tabsProjects, 'tabs__btn');
+
 })
+// burger
+const burger = document.querySelector('.burger');
+const nav = document.querySelector('.nav');
+burger.addEventListener('click', (e) => {
+  e.currentTarget.classList.toggle('burger--active');
+  nav.classList.toggle('active');
+});
 
+// open search form
+const searchActivate = () => {
+
+  document.querySelector('.form__btn').addEventListener('click', (e) => {
+    if (window.innerWidth < 999) {
+      e.preventDefault()
+      document.querySelector('.search__form').classList.toggle('active')
+    }
+    return false
+  })
+}
+
+window.addEventListener('resize', searchActivate())
